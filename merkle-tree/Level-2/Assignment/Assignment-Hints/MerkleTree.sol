b6ccb1c857efe394e85026ca516085f3bb00d06a
Azan Adnan <azanbutt2311@gmail.com>
2024-03-18 10:09:38 +0500

    added hints for level 2 final assignment

---
+++ merkle-tree/Level-2/Assignment/Assignment-Hints/MerkleTree.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MerkleProof {
+    /**
+     * function verify
+     * @param {bytes32[]} proof - An array of Merkle proof elements
+     * @param {bytes32} root - The Merkle tree root hash
+     * @param {bytes32} leaf - The leaf node to be verified
+     * @param {uint256} index - The index of the leaf node in the Merkle tree
+     * returns {bool} True if the leaf node is valid member of the set represented by the Merkle root, false otherwise.
+     */
+    function verify() public pure returns (/** @param */ bool) {
+        // ... other code
+        /**
+         * Computes the parent hash within the Merkle tree at the current iteration.
+         * 1. Concatenates the current hash and proof element using standardized ABI encoding.
+         * 2. Generates a secure hash of the combined data using the keccak256 function.
+         */
+        //hash = keccak256(abi.encodePacked(hash, proofElement));
+        // ... other code
+    }
+}
