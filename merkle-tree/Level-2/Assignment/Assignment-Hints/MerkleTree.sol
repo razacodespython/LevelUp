a57dfda1cc55f2c078985c53199fdf13018a86b9
Azan Adnan <azanbutt2311@gmail.com>
2024-03-18 10:14:45 +0500

    added instructions for assignments hints

--- merkle-tree/Level-2/Assignment/Assignment-Hints/MerkleTree.sol
+++ merkle-tree/Level-2/Assignment/Assignment-Hints/MerkleTree.sol
@@ -10,14 +10,13 @@ contract MerkleProof {
      * @param {uint256} index - The index of the leaf node in the Merkle tree
      * returns {bool} True if the leaf node is valid member of the set represented by the Merkle root, false otherwise.
      */
-    function verify() public pure returns (/** @param */ bool) {
-        // ... other code
-        /**
-         * Computes the parent hash within the Merkle tree at the current iteration.
-         * 1. Concatenates the current hash and proof element using standardized ABI encoding.
-         * 2. Generates a secure hash of the combined data using the keccak256 function.
-         */
-        //hash = keccak256(abi.encodePacked(hash, proofElement));
-        // ... other code
-    }
+    //   function verify(/* parameters */) public pure returns (returns bool) {
+    // ... other code
+    /**
+     * Computes the parent hash within the Merkle tree at the current iteration.
+     * 1. Concatenates the current hash and proof element using standardized ABI encoding.
+     * 2. Generates a secure hash of the combined data using the keccak256 function.
+     */
+    //hash = keccak256(abi.encodePacked(hash, proofElement));
+    // ... other code
 }
